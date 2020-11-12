4ac1aadbacfa1d4da8ebe9f412301f8ccbcb939f
Hugobilou <58144542+Hugobilou@users.noreply.github.com>
2020-11-12 15:30:25 +0100

    ajout audio travis

---
+++ commands/Audio/yeah.js
@@ -0,0 +1,26 @@
+const { MessageEmbed, MessageAttachment } = require('discord.js');
+
+module.exports.run = (client, msg, args) => {
+    msg.delete();
+    const attachment = new MessageAttachment('./audio/travis.mp3')
+    msg.channel.send(attachment);
+
+    const embed = new MessageEmbed()
+        .setAuthor(`${msg.author.username}`)
+        .setColor("#ffff00")
+        .setDescription(`**Action**: yeah\n**Channel**: ${msg.channel.name}`)
+        .setThumbnail(msg.author.avatarURL())
+        .setTimestamp();
+        
+    client.channels.cache.get('757678569668345976').send(embed);
+    console.log(this.help.name);
+};
+
+module.exports.help = {
+    name: 'yeah',
+    description: 'Envoie l\'audio de Travis Scott qui tombe !',
+    category: 'audio',
+    isUserAdmin: false,
+    permissions: false,
+    args: false,
+};
